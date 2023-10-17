39f60fb4b4c92b953bd3aa797239bf630546b08e
EvanTarango1 <129339190+EvanTarango@users.noreply.github.com>
2023-10-17 13:04:25 -0400

    Update toma4.js

--- toma4.js
+++ toma4.js
@@ -6,3 +6,47 @@ document.addEventListener('DOMContentLoaded', function() {
         navigation.classList.toggle('active');
     });
 });
+
+
+    // JavaScript for the first slideshow
+        var slideIndex1 = 0;
+        showSlides(1);
+
+        // JavaScript for the second slideshow
+        var slideIndex2 = 0;
+        showSlides(2);
+
+        function showSlides(slideshowNumber) {
+            var i;
+            var slides = document.querySelectorAll("#slideshow-container-" + slideshowNumber + " .slide");
+
+            // Hide all slides
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+
+            // Move to the next slide
+            if (slideshowNumber === 1) {
+                slideIndex1++;
+                if (slideIndex1 > slides.length) {
+                    slideIndex1 = 1;
+                }
+                slides[slideIndex1 - 1].style.display = "block";
+                setTimeout(function () { showSlides(1); }, 3000); // Adjust as needed
+            } else if (slideshowNumber === 2) {
+                slideIndex2++;
+                if (slideIndex2 > slides.length) {
+                    slideIndex2 = 1;
+                }
+                slides[slideIndex2 - 1].style.display = "block";
+                setTimeout(function () { showSlides(2); }, 3000); // Adjust as needed
+            }
+        }
+        
+       ffunction playPause() {
+        var video = document.querySelector('video');
+        if (video.paused)
+            video.play();
+        else
+            video.pause();
+    }
